0359677b925dff769558d461de8d918fbdb037e3
Daniel Francis <francis@sugarlabs.org>
2014-05-11 12:26:04 -0300

    Auto-review changes
    Signed-off-by: Daniel Francis <francis@sugarlabs.org>

--- data/content_script.js
+++ data/content_script.js
@@ -1,11 +1,7 @@
-setInterval(hide_like_notifs, 100);
-
-function hide_like_notifs() {
+setInterval(function () {
   var notifs = $('#fbNotificationsJewel li._33c');
-  notifs.each(function(){
-    hide_notif_if_like( $(this) );
-  });
-}
+  notifs.each(function () {hide_notif_if_like($(this));});
+}, 100);
 
 function hide_notif_if_like(notif) {
   var notif_meta = $.parseJSON( notif.attr('data-gt') );
